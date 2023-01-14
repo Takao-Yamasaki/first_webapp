9e2c1e1b1166ac342f5d00a10ba1e12c0035f819
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2023-01-14 21:16:30 +0900

    リクエストの処理

--- server.go
+++ server.go
@@ -5,16 +5,17 @@ import (
 	"net/http"
 )
 
-// ハンドラ: HTTPリクエストを受理して、それを処理する
-// Requestから情報を取り出して、ResponseWriterで書き込む
-func handler(writer http.ResponseWriter, request *http.Request) {
-	// レスポンスを生成
-	fmt.Fprintf(writer, "Hello World, %s!", request.URL.Path[1:])
+type MyHandler struct{}
+
+func (h *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
 }
 
 func main() {
-	// ルートが呼び出された時、ハンドラが起動する
-	http.HandleFunc("/", handler)
-	// ポート8080を監視するようサーバーを起動する
-	http.ListenAndServe(":8080", nil)
+	handler := MyHandler{}
+	server := http.Server{
+		Addr:    "127.0.0.1:8080",
+		Handler: &handler,
+	}
+	server.ListenAndServe()
 }
