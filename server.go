4c6b04da885e451510f5c49ffa4c5cc0a23152c8
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2023-01-15 19:03:44 +0900

    ハンドラのチェイン

--- server.go
+++ server.go
@@ -3,33 +3,33 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"reflect"
-	"runtime"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello")
+type HelloHandler struct{}
+
+func (h HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello!")
 }
 
-func log(h http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		name := runtime.FuncForPC(reflect.ValueOf(h).Pointer()).Name()
-		fmt.Println("Handler function called - " + name)
-		h(w, r)
-	}
+func log(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("Handler called - %T\n", h)
+		h.ServeHTTP(w, r)
+	})
 }
 
-func protect(h http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+func protect(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// TODO: ユーザーの権限を確認するコードを書く
-		h(w, r)
-	}
+		h.ServeHTTP(w, r)
+	})
 }
 
 func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
-	http.HandleFunc("/hello", protect(log(hello)))
+	hello := HelloHandler{}
+	http.Handle("/hello", protect(log(hello)))
 	server.ListenAndServe()
 }
